2d4b5134110ce9433988b5c9a707fff5cb053f84
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-12-04 03:37:00 -0300

    Move things around

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -15,36 +15,36 @@ fn process(input: String) {
         .map(|x| x.trim().parse::<i32>().unwrap())
         .collect();
 
-    println!("Original program:\n{:?}", &program);
-    println!("Performing replacements...", );
+    let noun = 12;
+    let verb = 2;
 
-    let initialized = initialize(program, 12, 2);
+    println!("With noun {} and verb {}, will run:\n{:?}", noun, verb, &program);
+    let initialized = initialize(&program, noun, verb);
 
-    println!("Running program:\n{:?}", &initialized);
-
-    println!("Output:\n{:?}", run_program(initialized));
+    println!("Output:\n{:?}", run_program(&initialized)[0]);
 }
 
-fn initialize(mut program: Vec<i32>, noun: i32, verb: i32) -> Vec<i32> {
-    program[1] = noun;
-    program[2] = verb;
+fn initialize(program: &Vec<i32>, noun: i32, verb: i32) -> Vec<i32> {
+    let mut initialized = program.clone();
+    initialized[1] = noun;
+    initialized[2] = verb;
 
-    program
+    initialized
 }
 
-fn run_program(initial_program: Vec<i32>) -> Vec<i32> {
+fn run_program(initial_program: &Vec<i32>) -> Vec<i32> {
     let mut instr_ptr: usize = 0;
     let mut program = initial_program.clone();
 
     while program[instr_ptr] != 99 {
-        program = run_opcode(program, instr_ptr);
+        program = run_opcode(&program, instr_ptr);
         instr_ptr += 4;
     }
 
     program
 }
 
-fn run_opcode(program: Vec<i32>, instr_ptr: usize) -> Vec<i32> {
+fn run_opcode(program: &Vec<i32>, instr_ptr: usize) -> Vec<i32> {
     assert!(program.len() >= instr_ptr + 3); // each opcode has 3 arguments
 
     if program[instr_ptr] == 1 {
@@ -75,21 +75,21 @@ mod tests {
 
     #[test]
     fn test_simple_add_opcode() {
-        assert_eq!(run_opcode([1,0,0,0].to_vec(), 0), [2,0,0,0].to_vec());
+        assert_eq!(run_opcode(&[1,0,0,0].to_vec(), 0), [2,0,0,0].to_vec());
     }
 
     #[test]
     fn test_simple_mul_opcode() {
-        assert_eq!(run_opcode([2,3,0,3].to_vec(), 0), [2,3,0,6].to_vec());
+        assert_eq!(run_opcode(&[2,3,0,3].to_vec(), 0), [2,3,0,6].to_vec());
     }
 
     #[test]
     fn test_complex_add_program() {
-        assert_eq!(run_program([1,1,1,4,99,5,6,0,99].to_vec()), [30,1,1,4,2,5,6,0,99].to_vec());
+        assert_eq!(run_program(&[1,1,1,4,99,5,6,0,99].to_vec()), [30,1,1,4,2,5,6,0,99].to_vec());
     }
 
     #[test]
     fn test_complex_mul_program() {
-        assert_eq!(run_program([2,4,4,5,99,0].to_vec()), [2,4,4,5,99,9801].to_vec());
+        assert_eq!(run_program(&[2,4,4,5,99,0].to_vec()), [2,4,4,5,99,9801].to_vec());
     }
 }
