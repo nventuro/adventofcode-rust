56fbbe85fd97e679491bae379a0f98bf82308a02
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-12-06 15:35:39 -0300

    Add first part of day 5

--- day-05/src/computer.rs
+++ day-05/src/computer.rs
@@ -27,8 +27,20 @@ impl Hardware {
   }
 
   // Reads the value of the nth parameter for the current instruction (0-based)
-  fn parameter(&self, nth: usize) -> i32 {
-    self.read(self.program_counter + nth + 1)
+  fn parameter(&self, nth: usize) -> (i32, ArgumentMode) {
+    let value = self.read(self.program_counter + nth + 1);
+
+    // In a string representation, the last two digits of the instruction value
+    // are the opcodes (i.e. opcodes go from 0 to 99). We first remove those.
+    let mode_indicators = self.read(self.program_counter) / 100;
+
+    // Then, the mode for the 0-based nth parameter is the nth digit from the
+    // right: the first parameter is the units, second tenths, and so on.
+    let mode_indicator = (mode_indicators / 10_i32.pow(nth.try_into().unwrap())) % 10;
+
+    let mode = mode_indicator.try_into().unwrap();
+
+    (value, mode)
   }
 
   fn relative_jump_forward(&mut self, distance: usize) {
@@ -52,12 +64,26 @@ impl Hardware {
     let parameters: Vec<Parameter> = instruction.parameter_types()
       .iter()
       .enumerate()
-      .map(|(index, parameter_type)|
+      .map(|(index, parameter_type)| {
+        let (argument, mode) = self.parameter(index);
+
         match parameter_type {
-          ParameterType::In => Parameter::In(self.read(to_address(self.parameter(index)))),
-          ParameterType::Out => Parameter::Out(to_address(self.parameter(index))),
+          ParameterType::In => {
+            Parameter::In(
+              match mode {
+                ArgumentMode::Immediate => argument,
+                ArgumentMode::Indexed => self.read(to_address(argument)),
+              }
+            )
+          },
+          ParameterType::Out => {
+            // Out parameters can only be indexed
+            assert_eq!(mode, ArgumentMode::Indexed);
+
+            Parameter::Out(to_address(argument))
+          },
         }
-      ).collect();
+      }).collect();
 
     // Run instruction
     instruction.exec(&parameters, self);
@@ -93,6 +119,26 @@ enum Parameter {
   Out(usize),
 }
 
+#[derive(PartialEq)]
+#[derive(Debug)]
+enum ArgumentMode {
+  Immediate, // The value itself is the argument
+  Indexed, // The value is the address of the argument
+}
+
+// Map indicators to argument modes
+impl TryFrom<i32> for ArgumentMode {
+    type Error = i32;
+
+    fn try_from(x: i32) -> Result<Self, Self::Error> {
+        match x {
+          0 => Ok(ArgumentMode::Indexed),
+          1 => Ok(ArgumentMode::Immediate),
+          _ => Err(x),
+        }
+    }
+}
+
 impl Parameter {
   // Extracts in-value or fails
   fn get_input(&self) -> i32 {
@@ -116,7 +162,8 @@ impl TryFrom<i32> for Instruction {
     type Error = i32;
 
     fn try_from(x: i32) -> Result<Self, Self::Error> {
-        match x {
+        // Opcodes go from 0 to 99, the rest of the value sets the argument modes
+        match x % 100 {
           1 => Ok(Instruction::Add),
           2 => Ok(Instruction::Mul),
           3 => Ok(Instruction::Prompt),
@@ -217,5 +264,12 @@ mod tests {
         let mut computer = Hardware::new(vec![2,4,4,5,99,0]);
         computer.run();
         assert_eq!(computer.memory, vec![2,4,4,5,99,9801]);
+    }
+
+    #[test]
+    fn test_mul_program_argument_modes() {
+        let mut computer = Hardware::new(vec![1002,4,3,4,33]);
+        computer.run();
+        assert_eq!(computer.memory, vec![1002,4,3,4,99]);
     }
 }
