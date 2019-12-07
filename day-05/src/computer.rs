484bd2c972f01d37b695d92e93f0aede9e6fb5ec
Nicolás Venturo <nicolas.venturo@gmail.com>
2019-12-07 17:07:53 -0300

    Add io tests

--- day-05/src/computer.rs
+++ day-05/src/computer.rs
@@ -316,47 +316,132 @@ impl Instruction {
 }
 
 #[cfg(test)]
-mod tests {
+mod instructions {
     use super::*;
 
-    #[test]
-    fn test_simple_add_instruction() {
-      let mut computer = Hardware::new_with_terminal(vec![1,0,0,0]);
+    fn step(program: Vec<i32>, expected_instr: Instruction, expected_memory: Vec<i32>) {
+      let mut computer = Hardware::new_with_terminal(program);
       let instruction = computer.step();
 
-      assert_eq!(instruction, Instruction::Add);
-      assert_eq!(computer.memory, vec![2,0,0,0]);
-      assert_eq!(computer.program_counter, 4);
+      assert_eq!(instruction, expected_instr);
+      assert_eq!(computer.memory, expected_memory);
     }
 
     #[test]
-    fn test_simple_mul_instruction() {
-      let mut computer = Hardware::new_with_terminal(vec![2,3,0,3]);
-      let instruction = computer.step();
+    fn add() { step(vec![1,0,0,0], Instruction::Add, vec![2,0,0,0]) }
 
-      assert_eq!(instruction, Instruction::Mul);
-      assert_eq!(computer.memory, vec![2,3,0,6]);
-      assert_eq!(computer.program_counter, 4);
+    #[test]
+    fn mul() { step(vec![2,3,0,3], Instruction::Mul, vec![2,3,0,6]) }
+}
+
+#[cfg(test)]
+mod programs {
+    use super::*;
+
+    fn run(program: Vec<i32>, expected_memory: Vec<i32>) {
+      let mut computer = Hardware::new_with_terminal(program);
+      computer.run();
+      assert_eq!(computer.memory, expected_memory);
     }
 
     #[test]
-    fn test_add_program() {
-        let mut computer = Hardware::new_with_terminal(vec![1,1,1,4,99,5,6,0,99]);
+    fn add() { run(vec![1,1,1,4,99,5,6,0,99], vec![30,1,1,4,2,5,6,0,99]) }
+
+    #[test]
+    fn negative_add() { run(vec![1101,100,-1,4,0], vec![1101,100,-1,4,99]) }
+
+    #[test]
+    fn multiply() { run(vec![2,4,4,5,99,0], vec![2,4,4,5,99,9801]) }
+
+    #[test]
+    fn multiply_with_argument_modes() { run(vec![1002,4,3,4,33], vec![1002,4,3,4,99]) }
+}
+
+#[cfg(test)]
+mod programs_io {
+    use super::*;
+
+    fn run_io<InputFn: FnMut() -> i32>(program: &Vec<i32>, input: InputFn, expected_output: Vec<i32>) {
+      let mut output_values = Vec::new();
+      {
+        let output = |value| output_values.push(value);
+
+        let mut computer = Hardware::new(program.clone(), input, output);
         computer.run();
-        assert_eq!(computer.memory, vec![30,1,1,4,2,5,6,0,99]);
+      }
+      assert_eq!(output_values, expected_output);
     }
 
     #[test]
-    fn test_mul_program() {
-        let mut computer = Hardware::new_with_terminal(vec![2,4,4,5,99,0]);
-        computer.run();
-        assert_eq!(computer.memory, vec![2,4,4,5,99,9801]);
+    fn basic_io() { run_io(&vec![3,0,4,0,99], || 5, vec![5]) }
+
+    #[test]
+    fn io_equal_position_mode() {
+      // Test if input equals 8
+      let program = vec![3,9,8,9,10,9,4,9,99,-1,8];
+      run_io(&program, || 7, vec![0]);
+      run_io(&program, || 8, vec![1]);
+      run_io(&program, || 9, vec![0]);
     }
 
     #[test]
-    fn test_mul_program_argument_modes() {
-        let mut computer = Hardware::new_with_terminal(vec![1002,4,3,4,33]);
-        computer.run();
-        assert_eq!(computer.memory, vec![1002,4,3,4,99]);
+    fn io_equal_immediate_mode() {
+      // Test if input equals 8
+      let program = vec![3,3,1108,-1,8,3,4,3,99];
+      run_io(&program, || 7, vec![0]);
+      run_io(&program, || 8, vec![1]);
+      run_io(&program, || 9, vec![0]);
+    }
+
+    #[test]
+    fn io_less_than_position_mode() {
+      // Test if input is less than 8
+      let program = vec![3,9,7,9,10,9,4,9,99,-1,8];
+      run_io(&program, || 6, vec![1]);
+      run_io(&program, || 7, vec![1]);
+      run_io(&program, || 8, vec![0]);
+    }
+
+    #[test]
+    fn io_less_than_immediate_mode() {
+      // Test if input is less than 8
+      let program = vec![3,3,1107,-1,8,3,4,3,99];
+      run_io(&program, || 6, vec![1]);
+      run_io(&program, || 7, vec![1]);
+      run_io(&program, || 8, vec![0]);
+    }
+
+    #[test]
+    fn io_jump_position_mode() {
+      // Test if input is true
+      let program = vec![3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9];
+      run_io(&program, || 0, vec![0]);
+      run_io(&program, || 1, vec![1]);
+      run_io(&program, || 2, vec![1]);
+    }
+
+    #[test]
+    fn io_jump_immediate_mode() {
+      // Test if input is true
+      let program = vec![3,3,1105,-1,9,1101,0,0,12,4,12,99,1];
+      run_io(&program, || 0, vec![0]);
+      run_io(&program, || 1, vec![1]);
+      run_io(&program, || 2, vec![1]);
+    }
+
+    #[test]
+    fn io_complex() {
+      // Test if input is less, equal or greater to 8
+      let program = vec![
+        3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+        1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+        999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+      ];
+
+      run_io(&program, || 6, vec![999]);
+      run_io(&program, || 7, vec![999]);
+      run_io(&program, || 8, vec![1000]);
+      run_io(&program, || 9, vec![1001]);
+      run_io(&program, || 10, vec![1001]);
     }
 }
